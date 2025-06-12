bd2bb5800f9a41c579c7683c78c3b33945bc6fe2
SirawitC <sirawitchok@hotmail.com>
2025-06-12 14:38:32 +0700

    feature: add demo.ipynb and bugs/typos fixed

--- stable_diffusion/ddpm.py
+++ stable_diffusion/ddpm.py
@@ -4,7 +4,7 @@ import numpy as np
 class DDPMSampler:
     def __init__(self, generator: torch.Generator, num_training_steps=1000, beta_start: float = 0.00085, beta_end: float = 0.0120):
         self.betas = torch.linspace(beta_start**0.5, beta_end**0.5, num_training_steps, dtype=torch.float32) ** 2
-        self.alphas = 1 - self.betas
+        self.alphas = 1.0 - self.betas
         self.alpha_cumprod = torch.cumprod(self.alphas, 0)
         self.one = torch.tensor(1.0)
 
--- stable_diffusion/model_loader.py
+++ stable_diffusion/model_loader.py
@@ -6,7 +6,7 @@ from diffusion import Diffusion
 import model_converter
 
 def preload_models_from_standard_weights(ckpt_path, device):
-    state_dict = model_converter.load_from_standrad_weights(ckpt_path, device)
+    state_dict = model_converter.load_from_standard_weights(ckpt_path, device)
 
     encoder = VAE_Encoder().to(device)
     encoder.load_state_dict(state_dict["encoder"], strict=True)
